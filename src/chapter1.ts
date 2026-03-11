c8c005efe1be7a29bb2f7f26a32dc876e6911ba3
dkdkwl <dkdkwl@naver.comm>
2026-03-11 22:05:43 +0900

    onebit-typescript-section2

---
+++ src/chapter1.ts
@@ -0,0 +1,18 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3 : number = -0.123;
+let num4 : number = 0.123;
+let num5 : number = -Infinity;
+let num6 : number = Infinity;
+let num7 : number = NaN;
+
+// string
+let str1: string = "hello";
+let str2 : string = 'world';
+let str3 : string = 'hello world';
+let str4 : string = `hello world ${num1}`;
+
+let null1 : null = null;
+
+let undefined1 : undefined = undefined;
